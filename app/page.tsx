5c7536fe0200d00a7af847f196b4482edb2e05fa
Cy Jay Herrera <herreracyjay24@gmail.com>
2024-10-05 00:45:35 +0800

    Made the navbar responsive. Initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,37 @@
 "use client"
-import './globals.css'
+import './globals.css';
+import { useState } from 'react';
 export default function Home() {
+  
+  const [smallScreen, isSmallScreen] = useState(false);
+
   return (
     <main className="bg-main_color h-screen px-10 py-10">
-      <div className="flex items-center justify-evenly w-full mb-4">
+
+      {/* MOBILE NAVBAR */}
+      <div className='max-md:flex md:hidden mb-4 justify-end items-center gap-4'>
+        <form className="w-3/5 ">   
+            <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div className="relative">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                </div>
+                <input type="search" id="default-search" className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 " placeholder="Search Dress..." required />
+                <button type="submit" className="text-white absolute end-2.5 bottom-1 bg-highlight_color font-medium rounded-lg text-sm px-2 py-1">Search</button>
+            </div>
+        </form>
+        <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-highlight_color" aria-controls="navbar-default" aria-expanded="false">
+          <span className="sr-only">Open main menu</span>
+          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="black" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+          </svg>
+        </button>
+      </div>
+
+      {/* NAVBAR */}
+      <div className="flex items-center justify-evenly w-full mb-4 max-md:hidden">
         <form className="w-1/4 ">   
             <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div className="relative">
